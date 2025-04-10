d438a87dfdf06a5b655a4d75315f91cf89761bbf
fed2828 <federico@virtualisoftware.com>
2025-04-10 18:36:57 +0200

    1.3.3

--- js/script.js
+++ js/script.js
@@ -207,16 +207,6 @@ document.addEventListener("DOMContentLoaded", () => {
     
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const mobileMenu = document.getElementById('mobileMenu');
-  
-    if (menuToggle && mobileMenu) {
-      menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('active');
-      });
-    }
-  });
   
   
 });
